68b84ebfecff72b70e03b9c86d718e8b30eb87ec
Cee-Jay Malacas <ceejaymalacas17@gmail.com>
2022-12-12 16:08:35 +0800

    ejs-mate added

--- seeds/index.js
+++ seeds/index.js
@@ -24,9 +24,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad expedita qui minus, quasi fuga quisquam nemo voluptate praesentium illum aspernatur! Quidem quae aliquam odit. Molestiae et autem tempore? Totam, aliquam?',
+            price
         })
         await camp.save();
     }
