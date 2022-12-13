94a3a6f86df67830120c5486cebeac84c6a2401e
Cee-Jay Malacas <ceejaymalacas17@gmail.com>
2022-12-13 23:45:49 +0800

    error handling incomplete

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate')
+const catchAsync = require('./utils/catchAsync')
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override')
 const Campground = require('./models/campground');
 
@@ -41,10 +43,14 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new')
 })
 
-app.post('/campgrounds', async (req, res) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save()
-    res.redirect(`/campgrounds/${campground._id}`)
+app.post('/campgrounds', async (req, res, next) => {
+    try {
+        const campground = new Campground(req.body.campground);
+        await campground.save();
+        res.redirect(`/campgrounds/${campground._id}`)
+    } catch (e) {
+        next(e)
+    }
 })
 
 app.get('/campgrounds/:id', async (req, res) => {
@@ -67,6 +73,10 @@ app.delete('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds')
+})
+
+app.use((err, req, res, next) => {
+    res.send('Oh boy, something went wrong!')
 })
 
 app.listen(3000, () => {
